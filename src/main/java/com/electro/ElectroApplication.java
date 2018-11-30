64e157e8e25b01ad36314d99664542cfb8074dc1
erdemakyildiz <erdem.akyildiz@hibizz.com>
2018-11-30 09:55:45 +0300

    remote

--- src/main/java/com/electro/ElectroApplication.java
+++ src/main/java/com/electro/ElectroApplication.java
@@ -2,11 +2,15 @@ package com.electro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ElectroApplication {
+public class ElectroApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ElectroApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(ElectroApplication.class);
+    }
 }
